f00638a0e21ca13dd3fb4c56de52fa143675bb62
Ferhat özçöllü <ferhat.ozcollu@hesapkurdu.com>
2021-08-03 23:08:45 +0300

    track

---
+++ odyssey-lift-off-part/client/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GlobalStyles from './styles';
+import Pages from './pages';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  /**
+   * this is allow to in memory caching.
+   * This enables it to store and reuse query results so it doesn't have to make as many network requests.
+   */
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <GlobalStyles />
+    <Pages />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
---
+++ odyssey-lift-off-part/client/src/pages/tracks.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Layout } from '../components';
+import { gql, useQuery } from '@apollo/client';
+import QueryResult from '../components/query-result';
+import TrackCard from '../containers/track-card';
+
+export const TRACKS = gql`
+  query getTracks {
+    tracksForHome {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+    }
+  }
+`;
+
+/**
+ * Tracks Page is the Catstronauts home page.
+ * We display a grid of tracks fetched with useQuery with the TRACKS query
+ */
+const Tracks = () => {
+  const { loading, error, data } = useQuery(TRACKS);
+  return (
+    <Layout grid>
+      <QueryResult error={error} loading={loading} data={data}>
+        {data?.tracksForHome?.map((track) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+  );
+};
+
+export default Tracks;
